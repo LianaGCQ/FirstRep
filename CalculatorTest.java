105c0e05c7c86aa37f804338ccb0ab31b7022e53
LianaGCQ <liana@gcq.ac.in>
2026-02-10 18:59:48 +0530

    Replace CalculatorTest with Test class

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,5 +1,8 @@
-package calculator;
-
-public class CalculatorTest {
-
+public class Test {
+ public static void main(String[] args) {
+   System.out.println("***************");
+   System.out.println("****  Hello from GitHub ****");
+   System.out.println("***************");
 }
+}
+
